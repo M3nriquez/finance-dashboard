4107d3e061b66177e49dd4ad5c1fd0f6e1ebcc19
unknown <0975961461cl@gmail.com>
2026-03-14 22:29:13 -0300

    fix: versao definitiva do servidor para o Render

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 require('./database'); 
 
 const authRoutes = require('./routes/authRoutes');
@@ -8,10 +8,10 @@ const transacoesRoutes = require('./routes/transacoesRoutes');
 const metasRoutes = require('./routes/metasRoutes');
 
 const app = express();
+
 const PORTA = process.env.PORT || process.env.PORTA || 3000; 
 
 app.use(express.json());
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,9 +19,12 @@ app.use('/', authRoutes);
 app.use('/', transacoesRoutes);
 app.use('/', metasRoutes); 
 
-
-app.get('/:path*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORTA, () => { 
